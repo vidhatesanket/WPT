0ac3cc356c3ecc00d79a72b3624236f0e5110a49
Vidhatesanket18 <sanketvidhate7745@gmail.com>
2023-12-09 10:01:48 +0530

    9/12

---
+++ assign7.js
@@ -0,0 +1,9 @@
+let ob={ director:"Rohit shetty" ,cast:{
+                                        hero:["Imran Hashmi","Akshay Kumar","Selmon Bhoi"],
+                                        heroine:["Sunny Leone","Esha Gupta","Janvi Kapoor"],
+                                        villain:["Sonu Sood"]
+                                    }
+                                    ,comedian:["Rajpal Yadav","Paresh Raval"]
+}
+console.log(ob)
+console.log(ob.cast.hero.length + ob.cast.heroine.length +ob.cast.villain.length  )
